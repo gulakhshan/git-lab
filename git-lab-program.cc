ec4250bbca8382a87dc5af5eb387e520b35ba907
gula <gh824514@0hio.edu>
2019-05-16 09:05:14 -0400

    Hell world!

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Gulakhshan hamad
+ *        Date: 05/17/2017
+ * Description: 1st code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
